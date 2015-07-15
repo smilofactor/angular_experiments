de9bacc84620a5800461f679c768b9a155927c08
Steve M <smilovidov@gmail.com>
2015-07-14 22:56:33 -0400

    Refactored for dependency injection

--- media/angular/app.js
+++ media/angular/app.js
@@ -1,5 +1,83 @@
 (function() {
 
+
+var app = angular.module('ngExp', ['store-products']);
+app.controller('StoreController', function() {
+      this.products = gems;
+
+  });
+
+
+app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+        this.review.createdOn = Date.now();
+        product.reviews.push(this.review);
+        this.review = {};
+    };
+});
+
+
+app.directive('productDescription', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'product-description.html'
+    };
+});
+
+
+app.directive('productSpecs', function() {
+    return {
+        restrict: 'A',
+        templateUrl: 'product-specs.html'
+    };
+});
+
+app.directive('productReviews', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'product-reviews.html'
+    };
+});
+
+
+/*
+app.directive('productGallery', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'product-gallery.html',
+        controller: function() {
+          this.current = 0;
+          this.setCurrent = function(imageNumber) {
+          this.current = imageNumber || 0;
+          }
+        },
+        controllerAs: 'gallery'
+    };
+});
+
+
+app.directive('productPanels', function() {
+    return{
+        restrict: 'E',
+        templateUrl: 'product-panels.html',
+        controller: function() {
+          this.tab = 1;
+
+          this.selectTab = function(setTab) {
+            this.tab = setTab;
+          };
+
+          this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+          };
+      },
+        controllerAs: 'panel'
+    };
+});
+*/
+
 var gems = [
     { 
     name: 'Azurite',
@@ -51,103 +129,5 @@ var gems = [
     ],
     }
 ];
-
-var app = angular.module('ngExp', []);
-app.controller('StoreController', function() {
-      this.products = gems;
-
-  });
-
-
-app.controller('PanelController', function() {
-  this.tab = 1;
-
-  this.selectTab = function(setTab) {
-    this.tab = setTab;
-  };
-
-  this.isSelected = function(checkTab) {
-    return this.tab === checkTab;
-  };
-
-  });
-
-
-app.controller('GalleryController', function() {
-    this.current = 0;
-    this.setCurrent = function(newGallery) {
-        this.current = newGallery || 0;
-    };
-
-});
-
-
-app.controller('ReviewController', function() {
-    this.review = {};
-
-    this.addReview = function(product) {
-        this.review.createdOn = Date.now();
-        product.reviews.push(this.review);
-        this.review = {};
-    };
-});
-
-
-app.directive('productGallery', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'product-gallery.html',
-        controller: function() {
-          this.current = 0;
-          this.setCurrent = function(imageNumber) {
-          this.current = imageNumber || 0;
-          }
-        },
-        controllerAs: 'gallery'
-    };
-});
-
-
-app.directive('productDescription', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'product-description.html'
-    };
-});
-
-
-app.directive('productSpecs', function() {
-    return {
-        restrict: 'A',
-        templateUrl: 'product-specs.html'
-    };
-});
-
-app.directive('productReviews', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'product-reviews.html'
-    };
-});
-
-app.directive('productPanels', function() {
-    return{
-        restrict: 'E',
-        templateUrl: 'product-panels.html',
-        controller: function() {
-          this.tab = 1;
-
-          this.selectTab = function(setTab) {
-            this.tab = setTab;
-          };
-
-          this.isSelected = function(checkTab) {
-            return this.tab === checkTab;
-          };
-      },
-        controllerAs: 'panel'
-    };
-});
-
 
 })();
